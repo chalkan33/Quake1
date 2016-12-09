461b0a90788a13a76f3f63eb153916f5ddc51a5c
Igor Guedes <ighyss@gmail.com>
2016-12-09 16:38:09 -0200

    Arquivo do programa finalizado

--- Parse.cpp
+++ Parse.cpp
@@ -4,7 +4,36 @@
 #include <map>
 using namespace std;
 
+void print_result(int game, map<string,int> players, map<string,int> mode, int totalkill){ //função para gerar os results
+	int flag_first = 1;
+	printf("game_%d: {\n", ++game);
+	printf("\ttotal_kills: %d;\n",totalkill);
+	printf("\tplayers: [");
 
+	for(map<string,int>::iterator it = players.begin(); it != players.end(); it++){ //iteraçao pelo mapa , vendo todos os player e printando eles
+		if(flag_first){
+			printf("\"%s\"",it->first.c_str());
+			flag_first = 0;
+		}
+		else
+			printf(", \"%s\"",it->first.c_str());
+	}
+	printf("]\n");
+	printf("\tkills: {\n");
+	flag_first = 1;
+	for(map<string,int>::iterator it = players.begin(); it != players.end(); it++)//iteraçao pelo mapa , vendo todos os player e printando eles
+		printf("\t\t\"%s\":%d,\n",it->first.c_str(), it->second-1);
+	printf("\t}\n");
+	printf("}\n");
+	
+
+	printf("\"game-%d\": {\n", game);
+	printf("\tkills_by_means: {\n");
+	for(map<string,int>::iterator it = mode.begin(); it != mode.end(); it++)////iteraçao pelo mapa , vendo todos os modos e printando eles
+		printf("\t\t\"%s\":%d,\n",it->first.c_str(), it->second);
+	printf("\t}\n");
+	printf("}\n");
+}
 
 void parse(){
 		int game = 0, totalkill = 0, firstGame = 1, comecou_jogo = 0;
